c4fe5cb62842b11e403137c123be48c133d4efa5
shakilkhan1414 <69055926+shakilkhan1414@users.noreply.github.com>
2020-08-27 23:45:15 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ $(document).ready(function(){
 
    $(".ham-menu").click(function(){
        $("nav ul").slideToggle();
-   })
-  });
+   });
 
-  $("#map .inputs a").click(function(){
+   $("#map .inputs a").click(function(){
     $(".alert-2").animate({right:"0",opacity:"1"},1000).animate({right:"0",opacity:"1"},2000).animate({right:"-25%",opacity:"0"},3000);
 });
 
-$("nav ul li:last-child").click(function(){
+    $("#home_btn").click(function(){
     $(".alert").animate({right:"0",opacity:"1"},1000).animate({right:"0",opacity:"1"},2000).animate({right:"-25%",opacity:"0"},3000);
 });
 
+
+  });
+
+ 
   
 $('.owl-carousel').owlCarousel({
     loop:true,
